d90a69d1135087f65f7ab90c676649cde8653c41
Mattias <90244980+MattiasHelgesson@users.noreply.github.com>
2023-12-07 09:14:34 +0100

    Bilder / News
    Laddade upp nya bilder och upptaderade news

--- src/NavBarChilds/News.js
+++ src/NavBarChilds/News.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import './News.css';
+import Arena_GameMode from '../images/2v2v2.png';
+import League_Items from '../images/League items.jpg';
+import Samira from '../images/Samira.jpg';
+import soul_fighter_viego_final from '../images/soul-fighter-viego-final.png';
 
 function News(props) {
   return (
     <div className="News">
       <div className="column">
-        <img src="image1.jpg" alt="Image 1" />
-        <p>Text for column 1</p>
+        <img src={League_Items} alt="" />
+        <h2>Patch 13.16</h2>
+        <p>Fakta om Patch 13.16 kommer här, Det kan vara allt från himmel till jord. Ignorera detta dock. Detta är bara för att fylla ut och vissa hur det kommer se ut med text i rutan</p>
       </div>
       <div className="column">
-        <img src="image2.jpg" alt="Image 2" />
-        <p>Text for column 2</p>
+        <img src={Samira} alt="" />
+        <h2>Tft Uppdates</h2>
+        <p>Fakta om tft uppdates kommer här. Nya saker som händer och nya karaktärs uppdelningar</p>
       </div>
       <div className="column">
-        <img src="image3.jpg" alt="Image 3" />
-        <p>Text for column 3</p>
+        <img src={Arena_GameMode} alt="" />
+        <h2>Arena Uppdate</h2>
+        <p>Nya saker som händer med game modes. Nya tillägg och gammla bortagelser. </p>
       </div>
       <div className="column larger-column">
-        <img src="image4.jpg" alt="Image 4" />
-        <p>Text for larger column 4</p>
+        <img src={soul_fighter_viego_final} alt="" />
+        <h2>Generell Info Som Hänt</h2>
+        <p>Bara lite text om vad som hänt. Saker som till kommit. Samt saker som hänt under League community. 
+Saker som kan vara användbara för spelaren eller bara generella saker som inte passar in någon annan stans</p>
       </div>
     </div>
   );
